f7ad7b1b7078f2e1cff8901a9d4edfcdd46f0899
quant-1729 <priyanshuagrawal1729@gmail.com>
2023-10-22 14:03:42 +0530

    this is commit for 22 october

--- actions/actions.py
+++ actions/actions.py
@@ -3,35 +3,52 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
-from typing import List, Dict, Text, Any
-
-from anaconda_navigator.api.external_apps.config_utils import Action
-from anaconda_navigator.external.UniversalAnalytics.Tracker import Tracker
-
-
+# from typing import List, Dict, Text, Any
+#
+# from anaconda_navigator.api.external_apps.config_utils import Action
+# from anaconda_navigator.external.UniversalAnalytics.Tracker import Tracker
 
 
 # This is a simple example for a custom action which utters "Hello World!"
-
- from typing import Any, Text, Dict, List
-
- from rasa_sdk import Action, Tracker
- from rasa_sdk.executor import CollectingDispatcher
-
-
-
-class ActionHelloWorld(Action):
+# from typing import Any, Text, Dict, List
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk.events import SlotSet
+#
+# # from rasa_sdk.events import Slotset
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+#
+#
+class ActionSetHostelSlot(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_Set_Hostel_slot"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        hostel_entry = next(tracker.get_latest_entity_values("requested_hostel"), None)
+        hostel_informations = next(tacker.get_latest_response)
+        utc = arrow.utcnow()
+        file_path_DJ= f"Responses/DJ.txt"
+        response_DJ = file_path_DJ.read()
 
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
-# class FormDataCollect(FormAction):
-
-
+        if hostel_entry is not None:
+            dispatcher.utter_message(f"SO you want to dive in the life of {hostel_entry}", response_DJ)
+            return [SlotSet("hostel", hostel_entry)]
+        else:
+            dispatcher.utter_message("I'm sorry, I didn't understand which hostel you mentioned.")
+            return []
+        return [SlotSet("hostel", hostel_entry)]
