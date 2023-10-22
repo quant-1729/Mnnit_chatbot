cdd317433ed82a6b0c4b773f49edbfebb99b0941
quant-1729 <priyanshuagrawal1729@gmail.com>
2023-10-22 22:42:19 +0530

    this is the best working model till 22 october 10:30 PM

--- actions/actions.py
+++ actions/actions.py
@@ -52,3 +52,49 @@ class ActionSetHostelSlot(Action):
             dispatcher.utter_message("I'm sorry, I didn't understand which hostel you mentioned.")
             return []
         return [SlotSet("hostel", hostel_entry)]
+
+class ActionProvideDirections(Action):
+    def name(self) -> Text:
+        return "action_provide_directions"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        location_name = tracker.get_slot("location")
+        destination_name = tracker.get_slot("destination")
+
+        location_coordinates = {
+            "Central Library": "25.49260751334134,81.86402110821898",
+            "Admin Building": "25.493425551166336,81.86273379235345",
+            "SVBH": "25.490837274599187, 81.86274481076372",
+            "Athletic Ground": "25.494390812450987,81.86448942334405",
+            "Dean Academics": "25.492371007273704,81.86276909854777"
+        }
+
+        destination_coordinates = {
+             "Central Library": "25.49260751334134,81.86402110821898",
+            "Admin Building": "25.493425551166336,81.86273379235345",
+            "SVBH": "25.490837274599187,81.86274481076372",
+            "Athletic Ground": "25.494390812450987,81.86448942334405",
+            "Dean Academics": "25.492371007273704,81.86276909854777"
+        }
+
+        if location_name in location_coordinates and destination_name in destination_coordinates:
+            location_coords = location_coordinates[location_name]
+            destination_coords = destination_coordinates[destination_name]
+
+            #Google Maps URL
+            map_url = f"https://www.google.com/maps/dir/?api=1&origin={location_coords}&destination={destination_coords}"
+
+            dispatcher.utter_message(
+                response="utter_provide_directions",
+                location=location_name,
+                destination=destination_name,
+                map_url=map_url
+            )
+        else:
+            dispatcher.utter_message(
+                response="utter_no_directions",
+                location=location_name,
+                destination=destination_name
+            )
+
+        return []
